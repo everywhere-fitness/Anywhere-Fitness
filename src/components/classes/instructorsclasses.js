331e702b08ff65041db913637c5b2fcb28e1e87c
markporo <markporo@yahoo.com>
2021-10-16 00:37:10 -0400

    mapping data in various components to disply class cards; linked through params to classDetails; fixed a few other things

--- src/components/classes/instructorsclasses.js
+++ src/components/classes/instructorsclasses.js
@@ -2,16 +2,51 @@
 //categories for each type of group fitness class 
 //they offer (yoga, insanity, RIPPED, pilates, etc.)
 
-import react from "react";
+import react, { useState } from "react";
 import "../../App.css"
+import { Link } from "react-router-dom";
+import { useHistory } from "react-router";
 
+const fakeInitialInstructorsClasses = [
+    {
+        name: "Jump Rope Zumba",
+        id: "1",
+        instructor: "Bob Jumper",
+        details: "Jump til your calve muscles explode",
+        img: "https://picsum.photos/200",
+        type: "aerobic",
+        time: "6:30 PST",
+        date: "Saturday Morning",
+        duration: "30 minutes",
+        intensity: "medium-high",
+        location: "Mobile",
+        currentNumberOfParticipants: "17",
+        maxClassSize: "50",
+    }
+];
 
 export default function InstructorsClasses() {
+    const [instructorsClasses, setInstructorsClasses] = useState(fakeInitialInstructorsClasses);
+    const [isFetchingInstructorsClasses, setIsFetchingInstructorsClasses] = useState(false);
+
+    const history = useHistory();
 
     return (
         <div>
-            <p>(shows all of the classes made/ taught by instructor)</p>
-            <button>Edit Button Next to each class once mapped..links to edit Form</button>
+            <div className="CardSection">
+                {isFetchingInstructorsClasses ? "Loading Classes..." :
+                    instructorsClasses.map((eachClass) => (
+
+                        <div className="classCard">
+                            <h2>{eachClass.name}</h2>
+                            <img src={eachClass.img} alt="coolImage" />
+                            <p>{eachClass.date} {eachClass.time}</p>
+                            <button onClick={() => { history.push('/editclass') }} className='classButton'>Edit</button>
+                            <Link to={`/details/${eachClass.id}`}> <button className='detailsButton'>See Details</button> </Link>
+                        </div>
+
+                    ))}
+            </div>
         </div>
     )
 }
