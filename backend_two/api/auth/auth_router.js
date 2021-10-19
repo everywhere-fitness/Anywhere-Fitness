edd7062266c2dcdfe6b11d7682d8d82559fd059b
Kyle Naumann <67936151+kyle-naumann@users.noreply.github.com>
2021-10-18 19:36:58 -0700

    built auth token

---
+++ backend_two/api/auth/auth_router.js
@@ -0,0 +1,19 @@
+const router = require("express").Router()
+const Users = require("../users/user_model")
+const bcrypt = require("bcryptjs")
+const tokenBuilder = require("./token_builder")
+
+
+router.post("/register", async (req, res, next) => {
+    const credentials = req.body
+    try{
+        const hash = bcrypt.hashSync(credentials.password, 8)
+        credentials.password = hash
+        let user = await Users.addUser(credentials)
+        const token = tokenBuilder(user)
+        res.status(201).json({ user, token })
+    }
+    catch(err){
+        next(err)
+    }
+})
