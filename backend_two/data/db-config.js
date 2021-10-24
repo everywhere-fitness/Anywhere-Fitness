898f92551e571d16e8cbcd1be003600322a0d903
Kyle Naumann <67936151+kyle-naumann@users.noreply.github.com>
2021-10-23 19:12:38 -0700

    created seperate repositories for FE/BE

--- backend_two/data/db-config.js
+++
@@ -1,6 +0,0 @@
-const configs = require("../knexfile");
-const knex = require("knex");
-
-const enviroment = process.env.NODE_ENV || "development";
-
-module.exports = knex(configs[enviroment]);
--- backend_two/index.js
+++
@@ -1,9 +0,0 @@
-require("dotenv").config();
-
-const server = require("./api/server");
-
-const { PORT } = require("./config");
-
-server.listen(PORT, () => {
-  console.log(`listening on ${PORT}`);
-});
